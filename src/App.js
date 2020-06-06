b7305ac47d5446ecf8d1aa52cd2157f1dbd98c56
rahil143 <pathanrahil143@gmail.com>
2020-06-06 13:06:43 +0530

    Added Route Skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React,{Component} from 'react';
+import {BrowserRouter as Router,Route,Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="container">
-      <h2>MERN-Stack TOdo App</h2>
-    </div>
-  );
+//Importing TodoList Componenets
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodoList from "./components/todo-list.component";
+
+import logo from "./logo.svg";
+
+class  App extends Component {
+  render(){ 
+    return (
+      <Router>
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export default class EditTodo extends Component{
+    render(){
+        return(
+            <div>
+                <p>Welcome to EditTodo List component</p>
+            </div>
+        );
+    }
+}
